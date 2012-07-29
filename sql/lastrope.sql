eb6bd99423ae80fbb4d70703595bfd83f50cf945
lastrope <famille_dudul@hotmail.fr>
2012-07-29 22:02:55 +0200

    mise  jour de la bdd

--- sql/lastrope.sql
+++ sql/lastrope.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le : Dim 29 Juillet 2012 à 17:03
--- Version du serveur: 5.5.16
--- Version de PHP: 5.3.8
+-- Généré le: Dim 29 Juillet 2012 à 20:01
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.3.13
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -33,7 +33,15 @@ CREATE TABLE IF NOT EXISTS `header` (
   `meta_keywords` text COLLATE utf8_unicode_ci NOT NULL,
   `lang` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`idHeader`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `header`
+--
+
+INSERT INTO `header` (`idHeader`, `title`, `meta_description`, `meta_keywords`, `lang`) VALUES
+(1, 'Lastrope', 'Le site du groupe de heavy métal progressif Lastrope', 'lastrope,heavy metal, progressif, iron maiden, dream theater,guitare, solo,batterie,php,html,css,mysql,css3,html5,jquery,javascript', 'fr'),
+(2, 'Lastrope', 'The website of the heavy metal progressif band Lastrope', 'lastrope,heavy metal, progressif, iron maiden, dream theater,guitar, solo,drum,php,html,css,mysql,css3,html5,jquery,javascript', 'en');
 
 -- --------------------------------------------------------
 
@@ -47,8 +55,23 @@ CREATE TABLE IF NOT EXISTS `links` (
   `href` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `lang` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`idLinks`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
 
+--
+-- Contenu de la table `links`
+--
+
+INSERT INTO `links` (`idLinks`, `text`, `href`, `lang`) VALUES
+(1, 'Actualités', 'news.php', 'fr'),
+(2, 'News', 'news.php', 'en'),
+(3, 'Bio', 'bio.php', 'fr'),
+(4, 'Bio', 'bio.php', 'en'),
+(5, 'Media', 'media.php', 'fr'),
+(6, 'Media', 'media.php', 'en'),
+(7, 'Son', 'son.php', 'fr'),
+(8, 'Sound', 'son.php', 'en'),
+(9, 'Contacte', 'contact.php', 'fr'),
+(10, 'Contact', 'contact.php', 'en');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
