0f07de1b3ff6e41d4b69775c36ce1e65915bb5cc
lastrope <franck.gautier.developpeur@gmail.com>
2012-07-29 12:12:24 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="Description" content="" >
+        <title>Lastrope</title>
+        <link rel="stylesheet" href="public/css/reset.css" type="text/css" />
+        
+        <script type="text/javascript" src="public/js/jquery.min.js"></script>
+        <link rel="icon" type="image/png" href="public/img/favicon.png" />
+        <!--[if IE]>
+         <link rel="shortcut icon" type="image/x-icon" href="img/favico.ico" />
+        <![endif]-->
+    </head>
+    <body>
+         
+    </body>
+</html>
