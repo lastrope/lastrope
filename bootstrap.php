cc245c12e6e5d619640f62219656c1a1cfbc1717
lastrope <franck.gautier.developpeur@gmail.com>
2012-07-30 20:53:43 +0200

    Ajout de la prise en compte de la langue

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,16 @@ define("SERVER_NAME","localhost");
 define("DATABASE","lastrope");
 define("USER","root");
 define("PASSWORD","");
+require_once 'class/Session.php';
 
-/** Main Connection to Database **/
+// création de l'objet de session
+$session = new Session();
+// Test si la session a déjà été initialisée
+if($session->read('langue') === null){
+    // En cas de non existance , création de la variable session en FR
+    $session->write('langue','fr');
+}
+
 try {
     $pdo = new PDO('mysql:dbname=' . DATABASE . ";host=" . SERVER_NAME, USER, PASSWORD);
 	$pdo->exec('SET NAMES utf8');
