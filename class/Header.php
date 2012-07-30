ca55aaf61bfed8d68a066c7f04ae8d6a268a76d9
lastrope <franck.gautier.developpeur@gmail.com>
2012-07-30 20:52:43 +0200

    Modification constructeur ( langue )

--- class/Header.php
+++ class/Header.php
@@ -5,17 +5,19 @@
  */    
 class Header{
     private $pdo = null;
+    private $lang = null;
     /**
      * constructor 
      * @param $pdo 
      */
-    public function __construct($pdo){
+    public function __construct($pdo, $lang){
         /** Set the connection database **/
         $this->pdo = $pdo;
+        $this->lang = $lang;
         return $this;
     }
-    public function getAllHeaderInformation($lang = 'fr'){
-        $request = " SELECT title, meta_description, meta_keywords, lang FROM header WHERE lang=\"$lang\"";
+    public function getAllHeaderInformation(){
+        $request = " SELECT title, meta_description, meta_keywords, lang FROM header WHERE lang=\"$this->lang\"";
         try{
             $result = $this->pdo->query($request);
             return $result->fetch(PDO::FETCH_ASSOC);
