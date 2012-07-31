4f77b2a3366a39c57e668944e0214f97af36ac5d
lastrope <famille_dudul@hotmail.fr>
2012-07-31 13:34:52 +0200

    ajout du scroll perso reglage probleme alignement et changement drapeau

--- accueil.php
+++ accueil.php
@@ -5,15 +5,51 @@
 		</p>
 	</div>
 	
-	<div id="article">
-		<article>
-			<div class="titre">Titre</div>
-			<div class="content_article">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum nisi eu mi euismod sed dignissim lorem ultricies. Suspendisse iaculis lobortis leo non tincidunt. Cras viverra enim sed ligula gravida scelerisque. Duis tempor, arcu sed sollicitudin placerat, ligula mauris volutpat turpis, eget suscipit velit augue sit amet nisi. Ut in ante risus, non accumsan metus. In facilisis mollis magna, et dictum lacus tempor eget. Ut orci libero, interdum quis convallis sed, tincidunt ut nisl.
-				<br/>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. Sed et fermentum diam. Curabitur dictum, massa vitae pharetra accumsan, ante diam volutpat leo, eget rutrum tortor tortor ac nisl. Cras dictum risus et sem suscipit condimentum. Quisque congue massa et eros tincidunt et pharetra neque dignissim. Donec scelerisque, ante id varius tempus, tortor velit fermentum nisi, at gravida sem odio eu tortor. Aliquam at diam eget erat iaculis blandit. 
-				</p>
-			</div>
-		</article>
+	<div id="article" class="jScrollbar3">
+		<div class="jScrollbar_mask">
+			<article>
+				<div class="titre">Titre</div>
+				<div class="content_article">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique tempus purus, sed tincidunt orci elementum a.
+						Fusce sodales congue ligula nec sodales. Aliquam erat volutpat.
+						In ullamcorper, libero porttitor tempus lobortis, diam diam euismod turpis, non dignissim libero urna quis elit.
+						Nulla a tellus nisi. Sed sodales porttitor malesuada. Duis tellus lorem, hendrerit et mattis quis, mollis at mauris.
+						Proin volutpat, arcu a rutrum feugiat, libero tellus tempus lorem, posuere elementum ligula quam quis orci.<br/><br/>
+						Mauris accumsan pellentesque elit vel aliquet. Nunc rutrum malesuada nisl vitae semper.
+						Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+						Suspendisse tincidunt volutpat tortor. Phasellus aliquet, mauris eget congue aliquam, felis nisl semper elit, ac cursus lacus augue id mi. Phasellus vitae gravida libero.
+						Proin suscipit ipsum id lectus congue posuere sed sed erat. Nunc fermentum blandit vehicula.
+						In tristique, diam in venenatis sodales, nunc arcu tempor est, vel porttitor sem tellus in purus.
+						Maecenas suscipit aliquam euismod. Morbi nec elit enim, in cursus diam. Suspendisse eget dui vitae mauris convallis consequat.
+						Praesent velit magna, commodo et sagittis in, lobortis in tortor.<br/><br/>
+						Nam eget semper tortor. Nulla porttitor neque et enim tincidunt suscipit.
+						Curabitur dictum interdum turpis, et porttitor nisl consectetur at. Praesent vel magna nisi. Nulla facilisi.
+						In et mauris et lacus consectetur dignissim vel sit amet leo. Sed dui dui, scelerisque vel molestie sed, iaculis eget lorem.
+						Maecenas vel dolor purus. Morbi non mi et libero dictum fringilla. Sed porttitor facilisis nulla, non congue eros blandit id.
+						Duis tempus, risus nec vulputate tristique, lacus ipsum ullamcorper dui, ut ornare dolor metus sit amet odio. Duis non ipsum urna, at ultrices mauris.<br/><br/>
+						Proin tristique placerat nulla vel consectetur. Proin eget mi ut mauris interdum rutrum.
+						Integer rutrum suscipit risus, at aliquet velit tempor ut. Proin eu erat in dui tincidunt tristique.
+						Aenean viverra velit sed urna mollis dictum. Nam dui odio, porta ut sagittis id, lobortis eu dui.
+						Nullam molestie iaculis ligula, a interdum lacus mollis non. Nam felis diam, accumsan vitae aliquam nec, euismod ut metus.
+						Integer tempus, risus non fermentum pharetra, lectus augue accumsan nulla, at rutrum ante elit eget lacus. Donec mattis velit ac libero interdum convallis.<br/><br/>
+						Nulla mollis lectus quis tortor feugiat molestie tincidunt magna vehicula.
+						Aenean eu tortor nibh. Integer tincidunt, purus at mattis luctus, urna dui volutpat nulla, sed fermentum ipsum metus at mi.
+						Nullam laoreet vehicula libero, id accumsan odio lacinia et. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nam vitae tellus erat, sit amet egestas mi. Praesent eu diam risus. Pellentesque iaculis, lorem et dictum mattis, neque odio imperdiet ante, quis consequat neque velit quis ante.
+						Morbi accumsan neque odio, non cursus ipsum. Cras commodo leo et nisl sollicitudin venenatis. Aliquam convallis condimentum interdum.
+						Duis ullamcorper, ligula et commodo semper, urna sem convallis dui, sit amet blandit justo orci non velit.
+						Suspendisse ligula ante, dignissim non lacinia ac, vestibulum semper erat. Sed ut ipsum purus, quis rutrum arcu.<br/><br/>
+					</p>
+				</div>
+			</article>
+		</div>
+		
+		<div class="jScrollbar_draggable">
+			<a href="#" class="draggable"></a>
+		</div>
+			
+		<div class="clr"></div>
 	</div>
 	
 	<div id="event">
