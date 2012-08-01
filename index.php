1a99890c27ca089e117b67fd0751f1e910883282
lastrope <franck.gautier.developpeur@gmail.com>
2012-08-01 21:19:58 +0200

    modification de l'include

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
 	require_once 'bootstrap.php';
 	require_once 'class/Header.php';
-
+        
 	$header = new Header($pdo, $session->read('langue'));
 	$headerInformation = $header->getAllHeaderInformation();
+        
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -22,7 +23,7 @@
 
             // Ici c'est le corp de la page. Il serait préférable de découper chaque partie pour avoir un code souple et modulable
             // Décommenter cette ligne et mettre entre guillemet le chemin relatif de la page à inclure
-            include("accueil.php");
+            include($page.".php");
             // La page a modifier pour le footer
             include("page/footer.phtml");
 		?>
