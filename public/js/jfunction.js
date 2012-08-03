5b59d48984c7e9b11b1b65f52c62b6924dcd47b3
lastrope <famille_dudul@hotmail.fr>
2012-08-03 18:32:19 +0200

    reglage prb lang

--- public/js/jfunction.js
+++ public/js/jfunction.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
 	$('#box').css({left: default_left});
 	$('#rectangle').css({width: default_width});
 	
-	
 	// Rollover menu
 	$('#header li.menu').hover(function(){
 		left = Math.round($(this).offset().left - $('#header').offset().left);
@@ -26,4 +25,59 @@ $(document).ready(function(){
 		$('#box').stop().animate({left:default_left},{duration:1000, easing:effet});
 		$('#rectangle').stop().animate({width:default_width},{duration:1000, easing:effet});
 	});
+	
+		// Responsive design function onload
+		$(window).load(function(){
+			if($('body').width() < 1200){
+				$('#welcome').css({
+					width:0,
+					height:0
+				});
+				$('#welcome').empty();
+				$('#article').css({
+					margin:'0 0 0 20px'
+				});
+			}else if ($('body').width() > 1200){
+				$('#welcome').css({
+					width:'300px',
+					height:'166px'
+				});
+				$('#welcome').html('<p>Bienvenue sur le site de Lastrope !</p>');
+				$('#article').css({
+					margin:'auto'
+				});
+			}
+			if($('body').height() < 700){
+				$('footer').empty();
+			}else if($('body').height() > 700){
+				$('footer').html('<div id="complement_back"><div id="footer"></div><div id="copyright"></div></div>');
+			}
+		})
+		// Responsive design function on resize
+		$(window).resize(function(){
+			if($('body').width() < 1200){
+				$('#welcome').css({
+					width:0,
+					height:0
+				});
+				$('#welcome').empty();
+				$('#article').css({
+					margin:'0 0 0 20px'
+				});
+			}else if ($('body').width() > 1200){
+				$('#welcome').css({
+					width:'300px',
+					height:'166px'
+				});
+				$('#welcome').html('<p>Bienvenue sur le site de Lastrope !</p>');
+				$('#article').css({
+					margin:'auto'
+				});
+			}
+			if($('body').height() < 700){
+				$('footer').empty();
+			}else if($('body').height() > 700){
+				$('footer').html('<div id="complement_back"><div id="footer"></div><div id="copyright"></div></div>');
+			}
+		});
 });
