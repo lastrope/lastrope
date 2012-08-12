e724683ccebe84c850fd91552813185b734f51d5
lastrope <franck.gautier.developpeur@gmail.com>
2012-08-12 12:05:10 +0200

    Affichage de la date modifié

--- news.php
+++ news.php
@@ -17,7 +17,7 @@
 	$tag_langue = isset($_SESSION['langue'])?$_SESSION['langue']:'fr';
 ?>
 <div id="conteneur">
-	<div id="selecteur" class="jScrollbar3">
+    	<div id="selecteur" class="jScrollbar3">
 		<ul style="float:left;display:block;">
 		<?php
 			// On veille à ne pas dépasser la taille des tableaux
@@ -75,7 +75,8 @@
 				<div class="titre <?php echo (isset($this_actu['type'])?'event_margin':''); ?>"><?php echo $this_actu['title']; ?></div>
 				<div class="content_article">
 					<p>
-					<?php echo $this_actu['body']; ?>
+					<?php echo nl2br($this_actu['body']."\n"); ?>
+					<?php echo "<p class='published'>".DATE_PUBLISHED." " , date("d/m/Y ".AT." H:i:s",$this_actu['date']) , "</p>"; ?>
 					</p>
 				</div>
 			</div>
@@ -95,7 +96,8 @@
 				<div class="titre <?php echo (isset($rest_array[$rest_key[$rest_cpt]]['type'])?'event_margin':''); ?>"><?php echo $rest_array[$rest_key[$rest_cpt]]['title']; ?></div>
 				<div class="content_article">
 					<p>
-					<?php echo $rest_array[$rest_key[$rest_cpt]]['body']; ?>
+					<?php echo nl2br($rest_array[$rest_key[$rest_cpt]]['body']."\n"); ?>
+					<?php echo "<p class='published'>".DATE_PUBLISHED." " , date("d/m/Y ".AT." H:i:s",$rest_array[$rest_key[$rest_cpt]]['date']) , "</p>"; ?>
 					</p>
 				</div>
 			</div>
