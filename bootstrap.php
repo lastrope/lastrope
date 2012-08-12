df2aded83721e3972b9b5578ebaa1aea0944f984
lastrope <franck.gautier.developpeur@gmail.com>
2012-08-12 12:03:47 +0200

    Ajout des gestion pour le loader et le mail statement

--- bootstrap.php
+++ bootstrap.php
@@ -21,7 +21,24 @@ require 'lang/' .$_SESSION['langue']. '.php';
 
 // Récupération du nom de la page
 $page = isset($_GET['page'])?$_GET['page']:'home';
-
+// Effacement de la variable de session mail_statement_* en cas de
+//  changement de page après envoie d'un message de contact
+if($page !=='contact'){
+    $session->write('mail_statement_confirm', '');
+    $session->write('mail_statement_error', '');
+}
+// LOADER 
+// Pour le faire uniquement au premier chargement de la page d'accueil et sur aucune autre page.
+if(!isset($_SESSION['loader'])){
+    if($page == 'home' ){
+	$session->write('loader','on');
+    }
+}else{
+    if($_SESSION['loader'] == 'on'){
+	$session->write('loader','off');
+    }
+}
+// Connexion à la base de données
 try {
     $pdo = new PDO('mysql:dbname=' . DATABASE . ";host=" . SERVER_NAME, USER, PASSWORD);
 	$pdo->exec('SET NAMES utf8');
