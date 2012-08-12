150db7d0f4c8f83f80370fd4755442273a3d728e
lastrope <franck.gautier.developpeur@gmail.com>
2012-08-12 12:05:26 +0200

    Ajout de la fonction loader

--- public/js/jfunction.js
+++ public/js/jfunction.js
@@ -37,3 +37,21 @@ $(document).ready(function(){
 		$('#rectangle').stop().animate({width:default_width},{duration:1000, easing:effet});
 	});
 });
+function loader(){
+    $('#corps').hide();
+    var  screenHeight = $(window).height()+"px";
+    var  halfScreen = ($(window).height()/2)+"px";
+    var logo_top_margin = (($(window).height()/2)-(155/2))+"px";
+    var logo_left_margin = (($(window).width()/2)-(155/2))+"px";
+    $('#loader').css('height',screenHeight);
+    $('#top_loader').css('height',halfScreen);
+    $('#bottom_loader').css('height',halfScreen);
+    $('#logo_loader').css('top',logo_top_margin);
+    $('#logo_loader').css('left',logo_left_margin);
+    $('#corps').fadeIn(4000);
+    $('#logo_loader').animate({'top':'42px'},1500).fadeOut(600);
+    $('#bottom_loader').animate({'height':'0px'},2500);
+    $('#top_loader').animate({'height':'0px'},2500);
+    
+
+}
