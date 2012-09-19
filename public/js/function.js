d7b3314742f366ea984b93293b29d14e01591612
fgautier <fgautier@vmichon-PC.stepnetfr.local>
2012-09-19 10:25:40 +0200

    module de recherche

--- public/js/function.js
+++ public/js/function.js
@@ -1,30 +1,34 @@
 function verif_search(){
-	var search = document.getElementById('search_value_id');
+    var search = document.getElementById('search_value_id');
 	
-	if(search.value == ""){
-		search.className = 'input_text_passive incorrect';
-		alert('Veuillez renseigner au moins un mot clef.');
-		return false;
-	} else {
-		search.className = 'input_text_passive';
-		return true;
-	}
+    if(search.value == ""){
+	search.className = 'input_text_passive incorrect';
+	alert('Veuillez renseigner au moins un mot clef.');
+	return false;
+    } else {
+	search.className = 'input_text_passive';
+	return true;
+    }
 }
 /** research **/
 $(document).ready(function(){
     var widthWindowOnLoad = (($(window).width()-555)/2);
-    $('#article').animate({'margin':'0 0 0 '+widthWindowOnLoad+'px'});
+    $('#article').animate({
+	'margin':'0 0 0 '+widthWindowOnLoad+'px'
+	});
     // A la saisie d'une lettre 
-    $('#search_value_id').live('keypress',function(e){
+    $('#search_value_id').bind('keypress',function(e){
 	var key = $(this).val().length;
 	// Si on backspace la saisie
 	if(e.which != 8){
 	    if(key > 1){
 		openPreview();
+		
 		researchPreview();
+		
 	    }else{
 		if($('#selecteur').css('left') == '-300px'){
-		   closePreview();
+		    closePreview();
 		}
 	    }
 	}else{
@@ -37,7 +41,7 @@ $(document).ready(function(){
     });
     // MAJ du contenu de l'encart si les domaines changes 
     $('.div_answer').live('click',function(){
-	    researchPreview();
+	researchPreview();
     });
     // Si l'on ferme le panel droite de recherche alors qu'il reste des caractères de saisi
     $('#image_back').live('click',function(){
@@ -53,29 +57,44 @@ $(document).ready(function(){
 });
 // Ouverture de l'encart de résultats pertinents'
 function openPreview(){
-    $('#selecteur').animate({'left':'-300px'},500);
-    $('#article').animate({'margin':'0 0 0 20px','width':'970px'},1000);
+    $('#selecteur').animate({
+	'left':'-300px'
+    },500);
+    $('#article').animate({
+	'margin':'0 0 0 20px',
+	'width':'970px'
+    },1000);
     $('#researchPreview').delay(1100).fadeIn();
 }
 // Fermeture de l'encart de résultats pertinents'
 function closePreview(){
     var width = (($(window).width()-555)/2); 
     $('#researchPreview').fadeOut();
-    $('#selecteur').delay(1000).animate({'left':'0'},1000);
-    $('#article').delay(500).animate({'margin':'0 0 0 '+width+'px','width':'555px'},500);
+    $('#selecteur').delay(1000).animate({
+	'left':'0'
+    },1000);
+    $('#article').delay(500).animate({
+	'margin':'0 0 0 '+width+'px',
+	'width':'555px'
+    },500);
 }
 // MAJ du contenu de l'encart 
 function researchPreview(){
+    
     var where = $("#type_search").val();
     var what = $("#search_value_id").val();
     $("#researchPreview").html('<img src="public/media/image/loader.gif" />');
     $.ajax({
 	type:'POST',
 	url:'script/search.php',
-	data:{'what':what,'where':where},
+	data:{
+	    'what':what,
+	    'where':where
+	},
 	success:function(response){
-	    $("#researchPreview").html(response);
+	    $("#researchPreview").delay(500).html(response);
 	    
 	}
     });
+    
 }
