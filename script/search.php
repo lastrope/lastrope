3a88d6274b38a49c3d270f1d5ed35fd291bd6348
fgautier <fgautier@vmichon-PC.stepnetfr.local>
2012-09-19 10:04:18 +0200

    module de recherche

--- script/search.php
+++ script/search.php
@@ -1,12 +1,11 @@
 <?php
-
 require_once '../bootstrap.php';
 require_once '../class/Research.php';
 $langue = $_SESSION['langue'];
 // Récupération des données du formulaire en supprimant les espaces aux extrémités
 $what = (isset($_POST['what'])) ? trim($_POST['what']) : null;
 $where = (isset($_POST['where'])) ? trim($_POST['where']) : null;
-
+$memoire = array();
 $research = new Research($pdo, $langue);
 $output = "";
 $output .= '<h3>Résultats pertinents</h3>';
@@ -14,40 +13,46 @@ if ($what != null && $where != null) {
     // séparation des tris de sélections
     $what_part = explode(' ', $what);
     $where_parts = explode(' ', $where);
+    // Pour chaque mot entré lors de la saisie
     foreach ($what_part as $what_value) {
-	foreach ($where_parts as $where_value) {
-	    switch ($where_value) {
-		case 'a_member':
-		    $request = "SELECT name, firstname FROM members WHERE (name LIKE '%$what_value%' ";
-		    $request .= "OR firstname LIKE '%$what_value%' ";
-		    $request .= "OR influences LIKE '%$what_value%' ";
-		    $request .= "OR short_desc LIKE '%$what_value%') ";
-		    $request .= "AND lang='$langue'";
-		    break;
-		case 'a_sound':break;
-		case 'a_video':break;
-		case 'a_news':
-		    $request = "SELECT title FROM actus WHERE (title LIKE '%$what_value%' ";
-		    $request .= "OR body LIKE '%$what_value%') ";
-		    $request .= "AND lang='$langue'";
-		    break;
-		case 'a_event':
-		    $request = "SELECT title FROM event WHERE (title LIKE '%$what_value%' ";
-		    $request .= "OR body LIKE '%$what_value%') ";
-		    $request .= "AND lang='$langue'";
-		    break;
-		default:;
-	    }
-	    
-	    $return = $research->execute($request);
-	    if (is_array($return)) {
-		$output .= '<p>';
-		foreach ($return as $result) {
-		    $output .= $result . ' ';
+	// Chaque domaine de recherche
+	if (trim($what_value) != '') {
+	    foreach ($where_parts as $where_value) {
+		// Construction de la requête en fonction du domaine
+		switch ($where_value) {
+		    case 'a_member':
+			$request = "SELECT concat(name,' ',firstname) as title FROM members WHERE (name LIKE '%$what_value%' ";
+			$request .= "OR firstname LIKE '%$what_value%' ";
+			$request .= "OR influences LIKE '%$what_value%' ";
+			$request .= "OR short_desc LIKE '%$what_value%') ";
+			$request .= "AND lang='$langue'";
+			break;
+		    case 'a_sound':break;
+		    case 'a_video':break;
+		    case 'a_news':
+			$request = "SELECT title FROM actus WHERE (title LIKE '%$what_value%' ";
+			$request .= "OR body LIKE '%$what_value%') ";
+			$request .= "AND lang='$langue'";
+			break;
+		    case 'a_event':
+			$request = "SELECT title FROM event WHERE (title LIKE '%$what_value%' ";
+			$request .= "OR body LIKE '%$what_value%') ";
+			$request .= "AND lang='$langue'";
+			break;
+		    default:;
 		}
-		$output .= '</p>';
+		// Exécution de la requête
+		$return = $research->execute($request);
+		if (is_array($return)) {
+		    // Construction de la sortie écran des résultats
+		    foreach ($return as $result) {
+			if (array_search($result['title'], $memoire) === FALSE) {
+			    $output .= '<p>' . $result['title'] . '</p>';
+			}
+			$memoire[] = $result['title'];
+		    }
+		}
 	    }
-	    
 	}
     }
     echo $output;
