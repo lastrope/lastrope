c515d24bceda3b212ebb60c6554d49ffaaedca3d
fgautier <fgautier@THINK-12-6.stepnetfr.local>
2013-01-08 15:17:26 +0100

    Maj Actualités + tests de player pour la page sound

--- sound.php
+++ sound.php
@@ -1,7 +1,47 @@
-<?php
+<div id="conteneur">
+    <div id="player-container">
+	<div class="item" style="width: 400px;" href="http://devreactor.com/audio/1.mp3">
+	    <div>
+		<div class="fr duration">02:06</div>
+		<div class="btn play"></div>
+		<div class="title">
+		    <b>The Ting Tings</b>
+		    - Shut up and let me go
+		</div>
+	    </div>
+	    <div class="player inactive"></div>
+	</div>
+	<div class="clear"></div>
+	<div class="item" style="width: 400px;" href="/public/media/music/Pub.MP3">
+	    <div>
+		<div class="fr duration">01:00</div>
+		<div class="btn play"></div>
+		<div class="title">
+		    <b>Pub oasis</b>
+		    - Test de music en relative
+		</div>
+	    </div>
+	    <div class="player inactive"></div>
+	</div>
+	<div class="clear"></div>
+	<div id="music-command">
+	    <a onclick="$('#player-container').playlist('play');" href="javascript:void(0);" id="play-btn">Play</a>
+	    <a onclick="$('#player-container').playlist('pause');" href="javascript:void(0);" id="stop-btn">Pause</a>
+	    <a onclick="$('#player-container').playlist('prev');" href="javascript:void(0);" id="prev-btn">Prev</a>
+	    <a onclick="$('#player-container').playlist('next');" href="javascript:void(0);" id="next-btn">Next</a>
+	</div>
+    </div>
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+</div>
+<script src="/public/js/drplayer.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+	
+	$("#player-container").playlist(
+	    {
+		playerurl: "/public/js/swf/drplayer.swf"
+	    }
+	);
+    });
+    
+</script>
